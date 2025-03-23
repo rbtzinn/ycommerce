7f95fb916cc4054445eb890ba6e389a54ae5b761
Roberto <roberto.gabriel2004@hotmail.com>
2025-03-23 15:23:43 -0300

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: true, // Minifica o código
-    chunkSizeWarningLimit: 1000, // Ajusta o limite de tamanho dos chunks
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'], // Separa o React em um chunk próprio
-          bootstrap: ['bootstrap'], // Separa o Bootstrap em um chunk próprio
-        },
-      },
-    },
-  },
-});
+})
