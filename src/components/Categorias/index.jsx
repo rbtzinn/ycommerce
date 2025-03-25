bcd7931d44cc50dbbb7169f708e726e05a813bcc
root <root@heaven.heaven.local>
2025-03-25 19:10:35 -0300

    Usando SDK nas categorias

--- src/components/Categorias/index.jsx
+++ src/components/Categorias/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import Sdk from "../../sdk"
-
+import './categorias.css'
 
 
 const Categorias = () => {
@@ -10,23 +10,29 @@ const Categorias = () => {
             const api = new Sdk();
             setCategories(await api.getCategories())
 
-
         }
         fetchData()
     })
     return (
-        <div className="container-dest bg-light rounded p-3">
+        <>
             <h5>Principais categorias</h5>
-            {
-                categories.map(c => {
-                    return (
-                        <span className="category" key={c.id}>
-                            <h4>{c.name}</h4>
-                        </span>
-                    )
-                })
-            }
-        </div>
+            <div className="container-dest   rounded p-3 categorias-container">
+
+                {
+                    categories.map(c => {
+                        return (
+
+                            <a className="categoria" key={`${c.id}`} href={`/categories/${c.slug}`}>
+                                <>
+                                    <div className="img" style={{ backgroundImage: `url('${c.icon}')` }}></div>
+                                </>
+                                <h4>{c.name}</h4>
+                            </a>
+                        )
+                    })
+                }
+            </div>
+        </>
     )
 }
 export default Categorias
